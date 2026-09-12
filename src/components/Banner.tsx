f71b8e80063fc460fdea4aaa736223668e57d581
mashukurmanilk <mashukurmanik2552@gmail.com>
2026-09-12 22:29:20 +0600

    completed the banner for mobile and desktop, added buttons

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
 import banner from '../assets/banner-stack.png';
 
 
 export default function Banner() {
   return (
-    <div className='flex flex-col md:flex-row items-center justify-between gap-8 p-4 sm:p-4 md:px-3 md:py-20 max-w-7xl my-0 mx-auto'>
+    <div className='flex flex-col md:flex-row items-center justify-between gap-8 p-4 sm:p-4 md:px-3 md:py-14 max-w-7xl my-0 mx-auto'>
         {/* Text-container */}
-        <div className='flex-1 text-center md:text-left'>
-            <h1 className='text-4xl md:text-6xl font-bold mb-4'>Build Your Ideal <br/><div className='text-5xl font-bold bg-gradient-to-r from-[#FF4500] via-[#E91E63] to-[#8A2BE2] bg-clip-text text-transparent'>Development Stack</div></h1>
+        <div className='flex-1 flex flex-col text-center md:text-left md:space-y-5'>
+            <h1 className='text-5xl md:text-6xl font-extrabold'>Build Your Ideal <br/><div className='text-5xl md:text-6xl font-extrabold bg-linear-to-r from-[#FD5425] via-[#DA1E77] to-[#AB2AB4] bg-clip-text text-transparent'>Development Stack</div></h1>
+            <p className='md:max-w-3xl text-xl text-gray-600 my-6'>Explore frontend, backend, database and tooling options,<br/>compare them side by side, and put together the stack that fits your next project</p>
+            <div className='flex justify-center md:justify-start my-10'>
+              <div className='flex flex-row justify-between gap-3'>
+                <button className="bg-linear-to-r from-[#FD5425] via-[#F46248] to-[#EC4897] text-white font-semibold py-3 px-4 rounded-xl transition duration-300 w-fit cursor-pointer">Explore Technologies</button>
+                <button className="py-3 px-12 border border-gray-200 rounded-xl transition duration-300 cursor-pointer">Learn More</button>
+              </div> 
+            </div>
         </div>
         {/* image-container */}
         <div className="flex-1 flex justify-center md:justify-end">
@@ -16,5 +22,3 @@ export default function Banner() {
     </div>
   );
 }
-
-
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Logo from '../assets/logo-text.png'
 import { RxHamburgerMenu } from "react-icons/rx";
 
@@ -7,17 +6,17 @@ export default function Nav() {
     <div className="w-full border-b border-gray-200 px-6 sm:px-12 py-3">
         <nav className="max-w-7xl mx-auto flex items-center justify-between relative">
             
-            {/* Left side- Hamburger menu (mobile) or Logo (desktop) */}
+            {/*Hamburger menu (mobile) or Logo (desktop) */}
             <div className="flex items-center">
-                <button className="md:hidden text-neutral-600 focus:outline-none translate-y-0.5">
+                <button className="md:hidden">
                     <RxHamburgerMenu
                         size="1.75rem"
                     />
                 </button>
-                <img className="hidden md:block w-[98.4px] h-7" src={Logo} alt="logo"/>
+                <img className="hidden md:block w-[98.4px] h-7 zoom-120" src={Logo} alt="logo"/>
             </div>
 
-            {/* Center- Logo (mobile) or Links (desktop) */}
+            {/*Logo (mobile) or Links (desktop) */}
             <div className="absolute max-w-full h-auto left-1/2 -translate-x-10/12 translate-y-1/12 md:static md:translate-x-0">
                 <img className="md:hidden w-[98.4px] h-7" src={Logo} alt="logo"/>
                 <ul className="hidden md:flex gap-6 items-center">
@@ -29,7 +28,7 @@ export default function Nav() {
                 </ul>
             </div>
 
-            {/* Right side- Buttons */}
+            {/*Buttons */}
             <div className="flex items-center gap-1 sm:gap-3">
                 <button className="btn btn-ghost btn-sm md:btn-md rounded-full text-xs sm:text-sm">Sign In</button>
                 <button className="btn btn-secondary btn-sm md:btn-md rounded-full text-xs sm:text-sm">Sign Up</button>
